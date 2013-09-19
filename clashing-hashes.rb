db734fddc3f51fef24e0d2036f99bddc35cdf9a8
Clay Stewart <claytonmstewart@gmail.com>
2013-09-19 09:22:22 -0500

    Updates to gems

--- clashing-hashes.rb
+++ clashing-hashes.rb
@@ -2,16 +2,13 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
+# for twitter api
+# require 'oauth'
+
 require_relative 'lib/twitter'
+# require_relative 'lib/twitter_connect'
 
 get '/' do
-  # @site_title = 'Clay Stew'
-  # @show_description = true
-  # if params[:flash] != ''
-  #   @flash = params[:flash]
-  # else 
-  #   @flash = nil
-  # end
   erb :index
 end
 
@@ -25,23 +22,19 @@ end
 post '/tweet' do
   if params[:twitter_handle] != ''
     @tweet_handle = params[:twitter_handle]
-    
   else 
     @tweet_handle = nil
   end
-
   erb :tweet
-
-  # if params[:tweet_handle] != nil
-  
-  # else 
-  #   params[:flash] = "You Must Provide a Twitter Handle!"
-  #   redirect '/'
-  # this is a good article on redirection: http://stackoverflow.com/questions/2727979/how-to-do-a-post-redirect-get-using-sinatra
-  # end
 end
 
 post '/app' do 
   content_type :json 
   "#{params}" 
 end 
+
+get '/test' do
+  # twit_connect = TwitterConnect.new
+  # @twit_connect = twit_connect.server_response
+  # puts "#{@twit_connect}"
+end
